dca68da6304034ae62aaf3dba9d9693e21138dc7
shivam2146 <shivam.mcs17.du@gmail.com>
2018-02-03 07:00:50 +0530

    update

--- src/bank/urls.py
+++ src/bank/urls.py
@@ -12,15 +12,21 @@ Including another URLconf
 """
 from django.urls import path
 #from django.views.generic import TemplateView
-from .views import home,logredi,ben_form,manHome,manAcc,manCus
+from .views import home,logredi,ben_form,manHome,manAcc,manCus,cusDet
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('cus/',home),
+    #path('cus/',home),
     path('login/',auth_views.login),
     path('login/logredi/',logredi),
-    path('cus/ben',ben_form),
+    path('logout/', auth_views.logout, {'next_page': '/'}),
+
+    path('cus-det/',cusDet),
+    #path('cus-tra/',cusTra),
+    path('cus-ben/',ben_form),
+
     path('man-home/',manHome),
     path('man-cus/',manCus),
-    path('logout/', auth_views.logout, {'next_page': '/'}),
     path('man-acc/',manAcc),
+
+
 ]
--- src/bank/views.py
+++ src/bank/views.py
@@ -24,7 +24,7 @@ def logredi(request):
             raise Http404("invalid user")
     elif request.user.groups.filter(name="Customer"):
         if Customer.objects.filter(cus_username=request.user).exists():
-            return redirect('/bank/cus/')
+            return redirect('/bank/cus-acc/')
         else:
             logout(request)
             raise Http404("invalid user")
@@ -62,7 +62,7 @@ def manCus(request):
         if BankManager.objects.filter(manager_username=request.user).exists():
             b = BankManager.objects.get(manager_username=request.user)
             obj = Customer.objects.filter(cus_bcode= b.manager_bcode)
-            return render(request,"manager_acc_detail.html",{'obj':obj})
+            return render(request,"manager_cus.html",{'obj':obj})
         else:
             logout(request)
             raise Http404("invalid user")
@@ -70,13 +70,35 @@ def manCus(request):
         logout(request)
         raise Http404("invalid user")
 
-
+@login_required(login_url="/bank/login/")
 def ben_form(request):
-    if request.method == "POST":
-        f = BenForm(request.POST)
-        ben = f.save(commit=False)
-        ben.ben_cusacc= 214146541632
-        ben.save()
-        return redirect("/bank/cus/")
-    form = BenForm()
-    return render(request,"ben_form.html",{'form':form})
+    if request.user.groups.filter(name="Customer"):
+        if Customer.objects.filter(cus_username=request.user).exists():
+            if request.method == "POST":
+                f = BenForm(request.POST)
+                ben = f.save(commit=False)
+                ben.ben_cusacc= 214146541632
+                ben.save()
+                return redirect("/bank/cus/")
+            else:
+                form = BenForm()
+                return render(request,"ben_form.html",{'form':form})
+        else:
+            logout(request)
+            raise Http404("invalid user")
+    else:
+        logout(request)
+        raise Http404("invalid user")
+
+@login_required(login_url="/bank/login/")
+def cusDet(request):
+    if request.user.groups.filter(name="Customer"):
+        if Customer.objects.filter(cus_username=request.user).exists():
+            obj= Customer.objects.get(cus_username=request.user)
+            return render(request,"cus_det.html",{'obj':obj,})
+        else:
+            logout(request)
+            raise Http404("invalid user")
+    else:
+        logout(request)
+        raise Http404("invalid user")
