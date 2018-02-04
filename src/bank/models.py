b22fc4770b3c228fa3c67c0e5d0d4fc475e41cbe
shivam2146 <shivam.mcs17.du@gmail.com>
2018-02-04 10:57:11 +0530

    add transaction model

--- src/bank/models.py
+++ src/bank/models.py
@@ -49,3 +49,10 @@ class Beneficiary(models.Model):
     Beneficiary_transfer_limit = models.DecimalField(max_digits=10, decimal_places=2)
     class Meta:
         unique_together = (('ben_cusacc', 'Beneficiary_account_no'),)
+
+class Transaction(models.Model):
+    from_acc  = models.CharField(max_length=34)
+    to_acc    = models.CharField(max_length=34)
+    amount    = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    date      = models.DateField(auto_now=True)
